2722ca562338f6b99618411ef5448b7bd8136eea
dnynurakhmad99 <80521202+dnynurakhmad99@users.noreply.github.com>
2022-07-02 22:18:27 +0700

    Add files via upload

---
+++ TheVehicles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheVehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreetRacer streetRacer = new StreetRacer();
+            FormulaOne formulaOne = new FormulaOne();
+            Helicopter helicopter = new Helicopter();
+
+            streetRacer.go();
+            formulaOne.go();
+            helicopter.fly();
+        }
+    }
+}
---
+++ TheVehicles/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheVehicles
+{
+    class Vehicle
+    {
+        public void go()
+        {
+            Console.WriteLine("I am driving");
+        }
+
+        public void fly()
+        {
+            Console.WriteLine("I am flying");
+        }
+    }
+}
